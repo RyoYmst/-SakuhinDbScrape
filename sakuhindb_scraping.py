46cf4d5e4fcd775af23d7d4588f286b2c5a89353
RyoYmst <k809231@kansai-u.ac.jp>
2015-09-07 14:49:52 +0900

    テキストデータに書き出し

--- sakuhindb_scraping.py
+++ sakuhindb_scraping.py
@@ -1,41 +1,64 @@
 #coding:utf-8
 
 from BeautifulSoup import BeautifulSoup
-# import sys
 import urllib2
-# import unicodedata
-# import re
-# import codecs
+import urllib
+import re
+import codecs
 
+
+def GetReviewNum(soup):
+	data = []
+	for i in soup.findAll("td",{"class":"center"}):
+		data.append(i)
+	return data[2].text
+
+def GetTitle(soup):
+	for i in soup.findAll("h1"):
+		return i.find(itemprop = "name").text
 
 def ExtractUrl():
 	url = "http://sakuhindb.com/jmanga/HUNTERXHUNTER/"
 	html = urllib2.urlopen(url)
 	soup = BeautifulSoup(html)
-	url_list = []
-	url_list.append(url + "1.html#sort_review")
+	title = GetTitle(soup)
+	review_num = GetReviewNum(soup)
+
+	url_data = []
+	url_data.append(url + "1.html#sort_review")
 	page_num = soup.findAll("ul",{"id":"pagination-flickr"})#対象コミックの前ページ数
 	for i in soup.findAll("ul",{"id":"pagination-flickr"}):
 		for link in i.findAll("a"):
-			url_list.append("http://sakuhindb.com" + link.get("href"))
-	return url_list
-
+			url_data.append("http://sakuhindb.com" + link.get("href"))
+	return url_data,title,review_num
 
 def ExtractReview(all_url):
 	review_text = []
-	html = urllib2.urlopen(all_url[0])
-	soup = BeautifulSoup(html)
-	for each_review in soup.findAll(itemprop = "reviewBody"):#.get_text()
-		review_text.append(each_review.text)
+	for i in range(0,1):
+	# for i in range(0,len(all_url)):
+		# print i
+		html = urllib2.urlopen(all_url[i])
+		soup = BeautifulSoup(html)
+		for each_review in soup.findAll(itemprop = "reviewBody"):#.get_text()
+			review_text.append(each_review.text)
 	return review_text
 
+def Output(title,review_num,review):
+	print review
+	write_data = codecs.open("masterthesis_review/"+ title +".txt","w","utf-8")
+	write_data.write(review_num)
+	for i in review:
+		write_data.write(i)
+	write_data.close()
 
 def main():
 	extract_url = ExtractUrl()#対象作品の全レビューページ
-	extract_review = ExtractReview(extract_url)
-	for i in extract_review:
-		print i
-    
+	extract_review = ExtractReview(extract_url[0])	
+	Output(extract_url[1],extract_url[2],extract_review)
+	
+	
+
+
 
 	
 
