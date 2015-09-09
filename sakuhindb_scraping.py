74fe0f0c6fe3edcf02ca4807fbe7f2f87979b79a
RyoYmst <k809231@kansai-u.ac.jp>
2015-09-09 10:22:39 +0900

    誤出力を修正

--- sakuhindb_scraping.py
+++ sakuhindb_scraping.py
@@ -17,50 +17,78 @@ def GetTitle(soup):
 	for i in soup.findAll("h1"):
 		return i.find(itemprop = "name").text
 
-def ExtractUrl():
-	url = "http://sakuhindb.com/jmanga/HUNTERXHUNTER/"
-	html = urllib2.urlopen(url)
-	soup = BeautifulSoup(html)
-	title = GetTitle(soup)
-	review_num = GetReviewNum(soup)
+def ExtractData(all_url,image_url):
+	data = []
 
-	url_data = []
-	url_data.append(url + "1.html#sort_review")
-	page_num = soup.findAll("ul",{"id":"pagination-flickr"})#対象コミックの前ページ数
-	for i in soup.findAll("ul",{"id":"pagination-flickr"}):
-		for link in i.findAll("a"):
-			url_data.append("http://sakuhindb.com" + link.get("href"))
-	return url_data,title,review_num
-
-def ExtractReview(all_url):
-	review_text = []
-	for i in range(0,1):
-	# for i in range(0,len(all_url)):
-		# print i
-		html = urllib2.urlopen(all_url[i])
+	for url,image in zip(all_url,image_url)[5:6]:
+		each_comic_data = {}
+		html = urllib2.urlopen(url)
 		soup = BeautifulSoup(html)
-		for each_review in soup.findAll(itemprop = "reviewBody"):#.get_text()
-			review_text.append(each_review.text)
-	return review_text
+		title = GetTitle(soup)
+		print title
+		review_num = GetReviewNum(soup)
+		each_comic_url = []
+		
+		each_comic_url.append(url + "1.html#sort_review")
+		page_num = soup.findAll("ul",{"id":"pagination-flickr"})#対象コミックの前ページ数
+		try:
+			urllib.urlretrieve(image,"../masterthesis_image/" + title + ".jpg")#画像抽出//画質荒すぎて使えない
+		except IOError:
+			print "例外検出"
 
-def Output(title,review_num,review):
-	print review
-	write_data = codecs.open("masterthesis_review/"+ title +".txt","w","utf-8")
-	write_data.write(review_num)
-	for i in review:
-		write_data.write(i)
-	write_data.close()
+		for i in soup.findAll("ul",{"id":"pagination-flickr"}):
+			for link in i.findAll("a"):
+				each_comic_url.append("http://sakuhindb.com" + link.get("href"))
+		each_comic_data["title"] = title
+		each_comic_data["review_num"] = review_num
+		each_comic_data["url"] = each_comic_url[:-1]#最後の値は次のページのURLなので無視
+		data.append(each_comic_data)
+
+	return data
+
+def ExtractReview(data):
+	all_review_text = []
+	for each_comic_data in data:
+		each_review_text = []
+		for each_comic_url in each_comic_data["url"]:
+			print each_comic_url
+		 	html = urllib2.urlopen(each_comic_url)
+			soup = BeautifulSoup(html)
+			for each_review in soup.findAll(itemprop = "reviewBody"):#.get_text()
+				#print each_review.text
+				each_review_text.append(each_review.text)
+		all_review_text.append(each_review_text)
+	return all_review_text
+
+def Output(data,reviews):
+	for i,each_comic_data,each_comic_review in zip(range(0,len(data)),data,reviews):
+		try:
+			write_data = codecs.open("../masterthesis_review/"+ data[i]["title"] +".txt","w","utf-8")
+			write_data.write(each_comic_data["review_num"]) 
+			for text in each_comic_review:
+				write_data.write(text)
+			write_data.close()
+		except IOError:
+			print "例外検出"
 
 def main():
-	extract_url = ExtractUrl()#対象作品の全レビューページ
-	extract_review = ExtractReview(extract_url[0])	
-	Output(extract_url[1],extract_url[2],extract_review)
+	link_url = []
+	image_url = []
+	url = "http://sakuhindb.com/anime/j_manga_popular.html"
+	html = urllib2.urlopen(url)
+	soup = BeautifulSoup(html)
 	
-	
+	for each_comic_data in soup.findAll("tr",{"class":"va_top"}):
+		url_data  = each_comic_data.find("a")["href"]
+		link_url.append("http://sakuhindb.com" + url_data)
+	for each_comic_image in soup.findAll("span",{"class":"like_link"}):
+		images = each_comic_image.findAll("img")
+		for image in images:
+			image_url.append(image["src"])
 
-
-
-	
+	extract_data = ExtractData(link_url,image_url)#対象作品の全レビューページ
+	extract_review = ExtractReview(extract_data)
+	Output(extract_data,extract_review)
 
 if __name__ == "__main__":
     main()
